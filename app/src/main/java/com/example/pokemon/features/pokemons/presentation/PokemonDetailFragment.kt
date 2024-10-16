2f2d734c3dc44dc2b1b7c040c1f3a2f152606be7
juandaalean <juanalean2525@gmail.com>
2024-10-16 15:30:44 +0200

    Añadida funcionalidad para poder ver un pokemon mediante el detail

--- app/src/main/java/com/example/pokemon/features/pokemons/presentation/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/pokemon/features/pokemons/presentation/PokemonDetailFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
 import com.example.pokemon.app.extensions.loadUrl
 import com.example.pokemon.databinding.FragmentPokemonDetailBinding
+import com.example.pokemon.features.pokemons.domain.Pokemon
 
 class PokemonDetailFragment: Fragment() {
+
+    private lateinit var pokemonFactory: PokemonFactory
+    private lateinit var viewModel: PokemonDetailViewModel
 
     private var _binding: FragmentPokemonDetailBinding? = null
     private val binding get() = _binding!!
@@ -27,6 +32,36 @@ class PokemonDetailFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        pokemonArgs.pokemonId
+        pokemonFactory = PokemonFactory(requireContext())
+        viewModel = pokemonFactory.buildPokemonDetailViewModel()
+        setupObserver()
+        pokemonArgs.pokemonId?.let {
+            viewModel.viewCreated(it)
+        }
+    }
+
+
+    private fun setupObserver(){
+        val pokemonObserver = Observer<PokemonDetailViewModel.UiState>{uiState ->
+            uiState.pokemon?.let {
+                bindData(it)
+            }
+            uiState.errorApp?.let {
+
+            }
+            if (uiState.isLoading){
+
+            } else{
+
+            }
+        }
+        viewModel.uiState.observe(viewLifecycleOwner, pokemonObserver)
+    }
+
+    private fun bindData(pokemon: Pokemon) {
+        binding.apply {
+            pokeimg1.loadUrl(pokemon.urlImage)
+            pokename1.text = pokemon.name
+        }
     }
 }
